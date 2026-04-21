f0bf8cd31b12bfa8267997e371881354005b2983
yukti patle <yuktipatle563@gmail.com>
2026-04-21 16:13:03 +0530

    fix ssl timeout

--- db.js
+++ db.js
@@ -7,7 +7,11 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
