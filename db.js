cc737bd5a705a36ba892a826fe255a9cd9b302f1
yukti patle <yuktipatle563@gmail.com>
2026-04-21 16:18:00 +0530

    use database_url

--- db.js
+++ db.js
@@ -1,18 +1,7 @@
 require("dotenv").config();
-
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
